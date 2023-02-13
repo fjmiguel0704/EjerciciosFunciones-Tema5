14528f036cd85087b1454da2fd695d181f7e70ff
fjmiguel0704 <fernandoj.miguel@iesnervion.es>
2023-02-13 12:50:24 +0100

    Arreglar fallo primera y última posición

---
+++ Tema5-EjerciciosFunciones/src/buscaminas/Main.java
@@ -0,0 +1,26 @@
+package buscaminas;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static Scanner read = new Scanner(System.in);
+
+	public static void main(String[] args) {
+
+		String tabla[] = { "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x",
+				"x", "x" };
+		String incognita[] = { "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x",
+				"x", "x" };
+
+		int posicionMina = 0;
+		int casillaUsuario = 0;
+
+		Funciones.generaMinas(tabla, posicionMina);
+		Funciones.pintaTablero(tabla);
+		System.out.println(Arrays.toString(tabla));
+		System.out.println(Arrays.toString(incognita));
+		Funciones.esGanador(casillaUsuario, incognita, tabla);
+	}
+
+}
