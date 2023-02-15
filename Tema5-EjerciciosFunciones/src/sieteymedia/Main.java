9622e4946a6bcccd045b86f8b08fd1da9cbf6ba6
fjmiguel0704 <fernandoj.miguel@iesnervion.es>
2023-02-15 14:39:16 +0100

    Con array

--- Tema5-EjerciciosFunciones/src/sieteymedia/Main.java
+++ Tema5-EjerciciosFunciones/src/sieteymedia/Main.java
@@ -1,10 +1,17 @@
 package sieteymedia;
 
+import java.util.Scanner;
+
+import javax.swing.plaf.basic.BasicTreeUI.TreeCancelEditingAction;
+
 public class Main {
+	public static Scanner read = new Scanner(System.in);
+	public static double tabla[] = { 1, 2, 3, 4, 5, 6, 7, 0.5, 0.5, 0.5 };
 
 	public static void main(String[] args) {
-		
-
+		Funciones.turnoJugador1(tabla);
+		Funciones.turnoJugador2(tabla);
+		Funciones.esGanador();
 	}
 
 }
